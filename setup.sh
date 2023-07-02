8ec5ea0a0d690cfbd1ebeefd2d96c41bad7da3a2
Yu Zhu <ross.zhu@outlook.com>
2023-07-02 02:16:09 -0600

    fixed init_task_weights

--- setup.sh
+++ setup.sh
@@ -9,18 +9,18 @@ ip="104.171.202.107"
 rsync -hrtv /home/yu/OneDrive/Call/remote-dev/data -e "ssh -i .ssh/lambda.pem" ubuntu@${ip}:~/call-remote
 
 # system update
-sudo apt-get update && sudo apt-get dist-upgrade
+sudo apt-get update && sudo apt-get dist-upgrade 
 
 # install python
-sudo apt install python3.11-full
-sudo apt install python3.11-dev
-sudo apt install python3.11-venv
-sudo apt install python3-pip
+# sudo apt install python3.11-full
+# sudo apt install python3.11-dev
+# sudo apt install python3.11-venv
+# sudo apt install python3-pip
 
 
 # install packages
 python3 -m pip install --upgrade pip
-pip3 install -U wandb deepspeed python-dotenv hydra-core
+pip3 install -U wandb deepspeed python-dotenv hydra-core lightning pyarrow transformers
 
 # run unit-test
 cd ~/Call/call-remote
