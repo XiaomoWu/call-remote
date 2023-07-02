69f019b7e3425065ac0e2d411436098b5aaa4514
Yu Zhu <ross.zhu@outlook.com>
2023-07-02 02:05:36 -0600

    Prepare for lambda

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ip="104.171.202.107"
+
+# ssh to remote server
+# ssh -i ~/.ssh/lambda.pem ubuntu@
+
+# copy data
+rsync -hrtv /home/yu/OneDrive/Call/remote-dev/data -e "ssh -i .ssh/lambda.pem" ubuntu@${ip}:~/call-remote
+
+# system update
+sudo apt-get update && sudo apt-get dist-upgrade
+
+# install python
+sudo apt install python3.11-full
+sudo apt install python3.11-dev
+sudo apt install python3.11-venv
+sudo apt install python3-pip
+
+
+# install packages
+python3 -m pip install --upgrade pip
+pip3 install -U wandb deepspeed python-dotenv hydra-core
+
+# run unit-test
+cd ~/Call/call-remote
+python code/v2/model/run.py experiment=unit-test
+
+# run wandb sweep
+wandb sweep /Call/call-remote/code/v2/model/configs/experiment/swp-frtxt.yaml
+
